888f865776fdbbe50bbcdf221c8d53897dfa2309
Dheeraj sai Kommuri <dheerajsaikommuri10@gmail.com>
2026-07-23 15:42:35 +0530

    Update bubble_sort.cpp

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,20 +1,33 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-void bubbleSort(vector<int>& v) {
-    int n = v.size();
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    // Print unsorted array
+    cout << "Unsorted Array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    // Bubble Sort
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
-            if (v[j] > v[j + 1])
-                swap(v[j], v[j + 1]);
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
         }
     }
-}
 
-int main() {
-    vector<int> v = {5, 1, 4, 2, 8};
-    bubbleSort(v);
-    for (auto i : v)
-        cout << i << " ";
+    // Print sorted array
+    cout << "\nSorted Array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
     return 0;
 }
